0284f4c3e8544f66e48a7222293b1888ba887160
Arkar Min <25-00286@dat.com>
2024-06-07 12:46:20 +0630

    Setup

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		fmt.Println(input)
+	}
+}
